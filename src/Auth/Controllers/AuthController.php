3569072c32887353656ae4b71b48c760b0e8c4e2
Folco Carril <folco.carril@gmail.com>
2026-09-23 18:42:12 -0300

    feat: auth controller added

--- src/Auth/Controllers/AuthController.php
+++ src/Auth/Controllers/AuthController.php
@@ -7,11 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
 class AuthController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response {
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
         if ($this->getUser()) {
             return $this->redirectToRoute('roles.index');
         }
@@ -27,13 +27,13 @@ class AuthController extends AbstractController
 
         return $this->render('auth/login.html.twig', array(
             'errors' => $errors,
-            'old'    => array('email' => $authenticationUtils->getLastUsername()),
+            'old' => array('email' => $authenticationUtils->getLastUsername()),
         ));
     }
 
-
     #[Route('/logout', name: 'logout', methods: array('POST'))]
-    public function logout(): void {
+    public function logout(): void
+    {
         throw new \LogicException('Logout lo maneja el firewall.');
     }
 }
